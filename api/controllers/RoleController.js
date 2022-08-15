21bc447bc403ad28772a8b2b9c6bffa74e726e07
Joadson Silva <joadson83@hotmail.com>
2022-08-15 18:57:02 -0300

    Methodo of the list all Roles - Método de listar todas as Roles

--- api/controllers/RoleController.js
+++ api/controllers/RoleController.js
@@ -1,6 +1,15 @@
+const model = require('../models');
+
 class RoleController {
 
-  
+  static async getAllRoles(req, res) {
+    try {
+      const roles = await model.Roles.findAll();
+      return res.status(200).json(roles);  
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = RoleController;
